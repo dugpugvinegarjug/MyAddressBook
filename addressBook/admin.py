9fdfd3b86eeaafb23f1d4369b9dd64a307a14893
Doug Sandilands <dougsandilands@gmail.com>
2024-01-25 14:00:55 -0500

    renamed Address table to AddressT

--- addressBook/admin.py
+++ addressBook/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Address
+from .models import AddressT
 
 # Register your models here.
 
-admin.site.register(Address)
+admin.site.register(AddressT)
 
 
--- addressBook/models.py
+++ addressBook/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Address(models.Model):
+class AddressT(models.Model):
 	name = models.CharField(max_length=200)
 	email = models.EmailField(max_length=200)
 	phone = models.CharField(max_length=15)
